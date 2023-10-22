04143a762a7775d1afbf4655006e25e69a30e1ce
trendSetters01 <trendycrypto01@gmail.com>
2023-10-22 19:44:50 -0400

    feat: added user registration address logs

--- handlers/commandHandlers/setaddress.js
+++ handlers/commandHandlers/setaddress.js
@@ -7,6 +7,9 @@ import {
 import { AddressSet, User } from "../../db/models/index.js";
 import { DB } from "../../db/config/index.js";
 
+import fs from "fs/promises";
+import path from "path";
+
 async function handleSetaddress(interaction) {
   const address = interaction.options.getString("address");
   const userId = interaction.user.id;
@@ -38,7 +41,8 @@ async function handleSetaddress(interaction) {
       // If everything was successful, commit the transaction
       await t.commit();
 
-      setUserAddress(userId, address);
+      await userAddressRegistration(userId, address);
+
       const setAddressEmbed = createSetAddressSuccessEmbed(address);
 
       await interaction.reply({ embeds: [setAddressEmbed] });
@@ -53,12 +57,45 @@ async function handleSetaddress(interaction) {
 
     // Here, we reply to the interaction with the failure embed, providing the error message.
     const failureEmbed = createSetAddressFailureEmbed(
-      error.message && 'Algorand address verification failed' || "An unexpected error occurred."
+      (error.message && "Algorand address verification failed") ||
+        "An unexpected error occurred."
     );
-    console.error('createSetAddressFailureEmbed: ',error.message)
+    console.error("createSetAddressFailureEmbed: ", error.message);
     await interaction.reply({ embeds: [failureEmbed], ephemeral: true });
   }
   // await logUsersAndAddressSets(userId);
+}
+
+async function userAddressRegistration(userId, address) {
+  setUserAddress(userId, address);
+
+  // Specify the directory and file paths
+  const dirPath = path.resolve("algorand", "phantomAssets", "addresses");
+  const filePath = path.join(dirPath, "phantomRegisteredUserAddresses.txt");
+
+  try {
+    // Check if the directory exists; if not, it will throw an error
+    await fs.access(dirPath);
+
+    // Read the current contents of the file
+    const data = await fs.readFile(filePath, "utf8");
+
+    // Split the contents by new line and remove any carriage return characters
+    const addresses = data
+      .split("\n")
+      .map((address) => address.trim().replace("\r", ""));
+
+    // Check if the address already exists in the array
+    if (!addresses.includes(address.trim())) {
+      // Address doesn't exist, so append it to the file
+      await fs.appendFile(filePath, `\n${address}`);
+      console.log("The address was appended to the file!");
+    } else {
+      console.log("setaddress: The address already exists in the file. No action taken.");
+    }
+  } catch (error) {
+    console.error("Error handling the address registration:", error);
+  }
 }
 
 async function logUsersAndAddressSets(userId) {
--- utils/handleNFTVerification.js
+++ utils/handleNFTVerification.js
@@ -17,17 +17,7 @@ async function handleNFTVerification(interaction, userChoice) {
       const isValid = await verifyNFT(userAddress);
 
       if (isValid) {
-        // Specify the directory path
-        const dirPath = path.resolve("algorand", "phantomAssets", "addresses");
-        const filePath = path.join(dirPath, "phantomVerifiedUserAddresses.txt");
-        try {
-          await fs.access(dirPath);
-        } catch (error) {
-          await fs.mkdir(dirPath, { recursive: true });
-        }
-        // If the NFT is verified, write the user's address to the file.
-        await fs.appendFile(filePath, `\n${userAddress}`);
-        console.log("The address was appended to the file!");
+        await addVerifiedUserAddress(userAddress);
 
         return userChoice === "versionOne"
           ? { verifynftEmbed: createVerifyV1NFTSuccessEmbed(), isValid }
@@ -52,6 +42,40 @@ async function handleNFTVerification(interaction, userChoice) {
       verifynftEmbed: sendSetAddressReminderForNFTVerificationEmbed(),
       isValid: false,
     };
+  }
+}
+
+async function addVerifiedUserAddress(userAddress) {
+  // Specify the directory path
+  const dirPath = path.resolve("algorand", "phantomAssets", "addresses");
+  const filePath = path.join(dirPath, "phantomVerifiedUserAddresses.txt");
+
+  try {
+    // Check if the directory exists; if not, it will throw an error
+    await fs.access(dirPath);
+
+    // Read the current contents of the file
+    const data = await fs.readFile(filePath, "utf8");
+
+    // Split the contents by new line and remove any carriage return characters
+    const addresses = data
+      .split("\n")
+      .map((address) => address.trim().replace("\r", ""));
+    // Check if the address already exists in the array
+    if (!addresses.includes(userAddress.trim())) {
+      // Address doesn't exist, so append it to the file
+      await fs.appendFile(filePath, `\n${userAddress}`);
+      console.log("The nft verified address was appended to the file!");
+    } else {
+      console.log(
+        "handleNFTVerification: The nft verified address already exists in the file. No action taken."
+      );
+    }
+  } catch (error) {
+    console.error(
+      "handleNFTVerification: Error handling the NFT verified address registration:",
+      error
+    );
   }
 }
 
