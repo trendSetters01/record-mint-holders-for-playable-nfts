5ffe3f402e137e24bc66b572100f04f06fac8b59
trendSetters01 <trendycrypto01@gmail.com>
2023-10-28 20:26:58 -0400

    feat(update): updated assets

--- algorand/phantomAssets/NFTs/v1.js
+++ algorand/phantomAssets/NFTs/v1.js
@@ -1,219 +1,275 @@
 export const v1NFTs = [
   {
     amount: 0,
-    "asset-id": 1184384233,
+    'asset-id': 1184384233,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785056,
+    'is-frozen': false,
+    'opted-in-at-round': 31785056
   },
   {
     amount: 0,
-    "asset-id": 1184385115,
+    'asset-id': 1184385115,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785087,
+    'is-frozen': false,
+    'opted-in-at-round': 31785087
   },
   {
     amount: 0,
-    "asset-id": 1184385570,
+    'asset-id': 1184385570,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785104,
+    'is-frozen': false,
+    'opted-in-at-round': 31785104
   },
   {
     amount: 0,
-    "asset-id": 1184386094,
+    'asset-id': 1184386094,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785124,
+    'is-frozen': false,
+    'opted-in-at-round': 31785124
   },
   {
     amount: 0,
-    "asset-id": 1184386316,
+    'asset-id': 1184386316,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785133,
+    'is-frozen': false,
+    'opted-in-at-round': 31785133
   },
   {
     amount: 0,
-    "asset-id": 1184386561,
+    'asset-id': 1184386561,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785143,
+    'is-frozen': false,
+    'opted-in-at-round': 31785143
   },
   {
     amount: 0,
-    "asset-id": 1184386826,
+    'asset-id': 1184386826,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785155,
+    'is-frozen': false,
+    'opted-in-at-round': 31785155
   },
   {
     amount: 0,
-    "asset-id": 1184387277,
+    'asset-id': 1184387277,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785171,
+    'is-frozen': false,
+    'opted-in-at-round': 31785171
   },
   {
     amount: 0,
-    "asset-id": 1184387558,
+    'asset-id': 1184387558,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785183,
+    'is-frozen': false,
+    'opted-in-at-round': 31785183
   },
   {
     amount: 0,
-    "asset-id": 1184387812,
+    'asset-id': 1184387812,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31785194,
+    'is-frozen': false,
+    'opted-in-at-round': 31785194
   },
   {
     amount: 0,
-    "asset-id": 1189279271,
+    'asset-id': 1189279271,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 31986364,
+    'is-frozen': false,
+    'opted-in-at-round': 31986364
   },
   {
     amount: 0,
-    "asset-id": 1196021734,
+    'asset-id': 1196021734,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32190469,
+    'is-frozen': false,
+    'opted-in-at-round': 32190469
   },
   {
     amount: 0,
-    "asset-id": 1196023401,
+    'asset-id': 1196023401,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32190514,
+    'is-frozen': false,
+    'opted-in-at-round': 32190514
   },
   {
     amount: 0,
-    "asset-id": 1196023815,
+    'asset-id': 1196023815,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32190537,
+    'is-frozen': false,
+    'opted-in-at-round': 32190537
   },
   {
     amount: 0,
-    "asset-id": 1199735459,
+    'asset-id': 1199735459,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32352649,
+    'is-frozen': false,
+    'opted-in-at-round': 32352649
   },
   {
     amount: 0,
-    "asset-id": 1201928642,
+    'asset-id': 1201928642,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32448701,
+    'is-frozen': false,
+    'opted-in-at-round': 32448701
   },
   {
     amount: 0,
-    "asset-id": 1202903739,
+    'asset-id': 1202903739,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32493827,
+    'is-frozen': false,
+    'opted-in-at-round': 32493827
   },
   {
     amount: 0,
-    "asset-id": 1202942383,
+    'asset-id': 1202942383,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32495042,
+    'is-frozen': false,
+    'opted-in-at-round': 32495042
   },
   {
     amount: 0,
-    "asset-id": 1202942684,
+    'asset-id': 1202942684,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32495052,
+    'is-frozen': false,
+    'opted-in-at-round': 32495052
   },
   {
     amount: 0,
-    "asset-id": 1202942942,
+    'asset-id': 1202942942,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32495061,
+    'is-frozen': false,
+    'opted-in-at-round': 32495061
   },
   {
     amount: 0,
-    "asset-id": 1202943080,
+    'asset-id': 1202943080,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32495069,
+    'is-frozen': false,
+    'opted-in-at-round': 32495069
   },
   {
     amount: 0,
-    "asset-id": 1202943233,
+    'asset-id': 1202943233,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32495076,
+    'is-frozen': false,
+    'opted-in-at-round': 32495076
   },
   {
     amount: 0,
-    "asset-id": 1216515186,
+    'asset-id': 1216515186,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32909429,
+    'is-frozen': false,
+    'opted-in-at-round': 32909429
   },
   {
     amount: 0,
-    "asset-id": 1216517880,
+    'asset-id': 1216517880,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32909475,
+    'is-frozen': false,
+    'opted-in-at-round': 32909475
   },
   {
     amount: 0,
-    "asset-id": 1216529084,
+    'asset-id': 1216529084,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32909886,
+    'is-frozen': false,
+    'opted-in-at-round': 32909886
   },
   {
     amount: 0,
-    "asset-id": 1217292588,
+    'asset-id': 1217292588,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32933601,
+    'is-frozen': false,
+    'opted-in-at-round': 32933601
   },
   {
     amount: 0,
-    "asset-id": 1217293452,
+    'asset-id': 1217293452,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32933621,
+    'is-frozen': false,
+    'opted-in-at-round': 32933621
   },
   {
     amount: 0,
-    "asset-id": 1217294634,
+    'asset-id': 1217294634,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32933647,
+    'is-frozen': false,
+    'opted-in-at-round': 32933647
   },
   {
     amount: 0,
-    "asset-id": 1217296122,
+    'asset-id': 1217296122,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32933681,
+    'is-frozen': false,
+    'opted-in-at-round': 32933681
   },
   {
     amount: 0,
-    "asset-id": 1217296722,
+    'asset-id': 1217296722,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 32933699,
+    'is-frozen': false,
+    'opted-in-at-round': 32933699
   },
   {
     amount: 0,
-    "asset-id": 1220250052,
+    'asset-id': 1220250052,
     deleted: false,
-    "is-frozen": false,
-    "opted-in-at-round": 33024021,
+    'is-frozen': false,
+    'opted-in-at-round': 33024021
   },
+  {
+    amount: 0,
+    'asset-id': 1224845342,
+    deleted: false,
+    'is-frozen': false,
+    'opted-in-at-round': 33165695
+  },
+  {
+    amount: 0,
+    'asset-id': 1224845524,
+    deleted: false,
+    'is-frozen': false,
+    'opted-in-at-round': 33165702
+  },
+  {
+    amount: 0,
+    'asset-id': 1224845821,
+    deleted: false,
+    'is-frozen': false,
+    'opted-in-at-round': 33165714
+  },
+  {
+    amount: 0,
+    'asset-id': 1224848535,
+    deleted: false,
+    'is-frozen': false,
+    'opted-in-at-round': 33165816
+  },
+  {
+    amount: 1,
+    'asset-id': 1226403545,
+    deleted: false,
+    'is-frozen': false,
+    'opted-in-at-round': 33217989
+  },
+  {
+    amount: 1,
+    'asset-id': 1226404600,
+    deleted: false,
+    'is-frozen': false,
+    'opted-in-at-round': 33218011
+  },
+  {
+    amount: 1,
+    'asset-id': 1226405478,
+    deleted: false,
+    'is-frozen': false,
+    'opted-in-at-round': 33218026
+  },
+  {
+    amount: 1,
+    'asset-id': 1226405764,
+    deleted: false,
+    'is-frozen': false,
+    'opted-in-at-round': 33218033
+  }
 ];
